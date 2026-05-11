83edb9b184581583c93c89758aa809c4dc92ecf8
Jean-Arnaud Ritouret <jeanarnaudritouret@Jean-Arnauds-MacBook-Pro.local>
2026-05-11 18:05:53 +0200

    remove name for privacy

--- dbt_aidn/models/staging/stg_patients.sql
+++ dbt_aidn/models/staging/stg_patients.sql
@@ -8,7 +8,6 @@ raw_patients as (
 
 select
     cast(patient_id          as varchar)     as patient_id,
-    cast(name                as varchar)     as name,
     cast(primary_provider_id as varchar)     as primary_provider_id,
     cast(postcode            as varchar)     as postcode,
     cast(updated_at          as timestamptz) as updated_time,
